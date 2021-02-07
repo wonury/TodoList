7e356bf6104fdc56b6356759ae4bde3e6c8eacd4
jinhee <hanmajoro97@thinktubekorea.com>
2021-02-07 11:51:50 +0900

    데거힐트 설정

--- app/src/main/java/com/thepencil/journel/MainApplication.java
+++ app/src/main/java/com/thepencil/journel/MainApplication.java
@@ -2,8 +2,9 @@ package com.thepencil.journel;
 
 import android.app.Application;
 
+import dagger.hilt.android.HiltAndroidApp;
 import timber.log.Timber;
-
+@HiltAndroidApp
 public class MainApplication extends Application {
     @Override
     public void onCreate() {
